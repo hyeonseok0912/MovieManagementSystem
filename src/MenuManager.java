11f5e1a83b49faf4aa230568868b16c17ac8f3be
hyeonseok <snrnakat@naver.com>
2021-03-14 15:09:36 +0900

    removed hello.java and added menumanager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,12 @@
+
+public class MenuManager {
+	public static void main(String[] args)  {
+		System.out.println("1. Add Movies");
+		System.out.println("2. Delete Movies");
+		System.out.println("3. Edit Movies");
+		System.out.println("4. View Movies");
+		System.out.println("5. Exit Movies");
+
+	}
+
+}
