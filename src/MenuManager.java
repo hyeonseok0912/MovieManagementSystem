4a8913590b455e1f1575ff874f4bdc8590c3615b
hyeonseok <snrnakat@naver.com>
2021-03-19 17:44:13 +0900

    Add the delete function and modify function

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -20,12 +20,27 @@ public class MenuManager {
 				int movienumber = input.nextInt();
 				System.out.print("Movie Name:");
 				String moviename = input.next();
+				System.out.println(movienumber+", "+moviename);
 				break;
+				
 			case 2:
+				System.out.print("Delete Movie Number:");
+				int movienumber2 = input.nextInt();
+				System.out.print("Delete Movie Name:");
+				String moviename2 = input.next();
+				break;
+				
 			case 3:
+				System.out.print("Modify Movie Number:");
+				movienumber = input.nextInt();
+				System.out.print("Modify Movie Name:");
+				moviename = input.next();
+				System.out.println(movienumber+", "+moviename);
+				continue;	
+				
 			case 4:
 				System.out.print("Movie Number:");
-				int movienumber2 = input.nextInt();
+				int movienumbr4 = input.nextInt();
 			}
 	}
 
